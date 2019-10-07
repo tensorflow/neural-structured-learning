8650926bc2999932a3de95710415052694a3680c
Chun-Sung Ferng <csferng@google.com>
2019-10-07 15:52:38 -0700

    Adds version.py and nsl.__version__
    This change creates `version.py` to hold the version information, which is
    shared to `nsl.__version__` and `setup.py`.
    
    Fixes #25
    
    PiperOrigin-RevId: 273395360

--- neural_structured_learning/__init__.py
+++ neural_structured_learning/__init__.py
@@ -4,3 +4,4 @@ from neural_structured_learning import configs
 from neural_structured_learning import keras
 from neural_structured_learning import lib
 from neural_structured_learning import tools
+from neural_structured_learning.version import __version__
