c649fb8aad03e8d3057903dfa585cb02ce478f2d
Arjun Gopalan <arjung@google.com>
2019-10-15 10:31:33 -0700

    Create NSL version 1.1.0. See RELEASE.md for a list of changes that are part of this release.
    PiperOrigin-RevId: 274835459

--- neural_structured_learning/version.py
+++ neural_structured_learning/version.py
@@ -15,8 +15,8 @@
 
 # We follow Semantic Versioning (https://semver.org/).
 _MAJOR_VERSION = '1'
-_MINOR_VERSION = '0'
-_PATCH_VERSION = '1'
+_MINOR_VERSION = '1'
+_PATCH_VERSION = '0'
 
 _VERSION_SUFFIX = ''
 
