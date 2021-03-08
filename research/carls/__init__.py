f5893dae333bb6932955b25737031511090d4f20
Neural-Link Team <neural-link@google.com>
2021-03-08 09:08:27 -0800

    Expose more modules for CARLS.
    PiperOrigin-RevId: 361580167

--- research/carls/__init__.py
+++ research/carls/__init__.py
@@ -1,10 +1,29 @@
 """CARLS APIs for Neural Structured Learning."""
 
+from research.carls import dynamic_embedding_config_pb2
+from research.carls import dynamic_embedding_neighbor_cache
+from research.carls import dynamic_embedding_ops
+from research.carls import kbs_server_helper_pybind as kbs_server_helper
+from research.carls import neighbor_cache_client
 from research.carls.dynamic_embedding_neighbor_cache import DynamicEmbeddingNeighborCache
+from research.carls.dynamic_embedding_ops import dynamic_embedding_lookup
+from research.carls.dynamic_embedding_ops import dynamic_embedding_update
 from research.carls.graph_regularization import GraphRegularizationWithCaching
+from research.carls.kbs_server_helper import KbsServerHelper
+from research.carls.kbs_server_helper import KnowledgeBankServiceOptions
 from research.carls.neighbor_cache_client import NeighborCacheClient
 
 __all__ = [
-    'GraphRegularizationWithCaching', 'NeighborCacheClient',
-    'DynamicEmbeddingNeighborCache'
+    'dynamic_embedding_config_pb2',
+    'dynamic_embedding_neighbor_cache',
+    'dynamic_embedding_ops',
+    'kbs_server_helper',
+    'neighbor_cache_client',
+    'DynamicEmbeddingNeighborCache',
+    'dynamic_embedding_lookup',
+    'dynamic_embedding_update',
+    'GraphRegularizationWithCaching',
+    'KbsServerHelper',
+    'KnowledgeBankServiceOptions',
+    'NeighborCacheClient',
 ]
---
+++ research/carls/kernels/__init__.py
@@ -0,0 +1,5 @@
+"""Tensorflow Custom Ops for CARLS."""
+
+from research.carls.kernels import gen_dynamic_embedding_ops
+
+__all__ = ['gen_dynamic_embedding_ops']
