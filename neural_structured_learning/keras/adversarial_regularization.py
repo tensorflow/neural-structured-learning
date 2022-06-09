47d66e23a35a99eaea3ac44346ba33bf34a23bc5
Chun-Sung Ferng <csferng@google.com>
2022-06-09 12:38:41 -0700

    Call base_model.save() for backward compatibility.
    The save() interface has changed in different TF versions. Since NSL
    supports back to TF 1.15, it is better to accept variadic arguments.
    
    Also updated the example notebook following internal review comments.

--- neural_structured_learning/keras/adversarial_regularization.py
+++ neural_structured_learning/keras/adversarial_regularization.py
@@ -23,10 +23,6 @@ import types
 
 
 import attr
-
-from keras.saving import save
-from keras.utils import traceback_utils
-
 import neural_structured_learning.configs as nsl_configs
 from neural_structured_learning.lib import adversarial_neighbor
 import six
@@ -699,58 +695,11 @@ class AdversarialRegularization(tf.keras.Model):
         scaled_adv_loss, name='scaled_adversarial_loss', aggregation='mean')
     return outputs
 
-
-  @traceback_utils.filter_traceback
-  def save(self,
-           filepath,
-           overwrite=True,
-           include_optimizer=True,
-           save_format=None,
-           signatures=None,
-           options=None,
-           save_traces=True):
-    # pylint: disable=line-too-long
-    """Saves the model to Tensorflow SavedModel or a single HDF5 file.
-    Please see `tf.keras.models.save_model` or the
-    [Serialization and Saving guide](https://keras.io/guides/serialization_and_saving/)
-    for details.
-    Args:
-        filepath: String, PathLike, path to SavedModel or H5 file to save the
-            model.
-        overwrite: Whether to silently overwrite any existing file at the
-            target location, or provide the user with a manual prompt.
-        include_optimizer: If True, save optimizer's state together.
-        save_format: Either `'tf'` or `'h5'`, indicating whether to save the
-            model to Tensorflow SavedModel or HDF5. Defaults to 'tf' in TF 2.X,
-            and 'h5' in TF 1.X.
-        signatures: Signatures to save with the SavedModel. Applicable to the
-            'tf' format only. Please see the `signatures` argument in
-            `tf.saved_model.save` for details.
-        options: (only applies to SavedModel format)
-            `tf.saved_model.SaveOptions` object that specifies options for
-            saving to SavedModel.
-        save_traces: (only applies to SavedModel format) When enabled, the
-            SavedModel will store the function traces for each layer. This
-            can be disabled, so that only the configs of each layer are stored.
-            Defaults to `True`. Disabling this will decrease serialization time
-            and reduce file size, but it requires that all custom layers/models
-            implement a `get_config()` method.
-    Example:
-    ```python
-    from keras.models import load_model
-    model.save('my_model.h5')  # creates a HDF5 file 'my_model.h5'
-    del model  # deletes the existing model
-    # returns a compiled model
-    # identical to the previous one
-    model = load_model('my_model.h5')
-    ```
-    """
-    # pylint: enable=line-too-long
-    save.save_model(self.base_model, filepath, overwrite, include_optimizer, save_format,
-                    signatures, options, save_traces)
-
-
-
+  def save(self, *args, **kwargs):
+    """Saves the base model. See base class for details of the interface."""
+    # Adversarial regularization doesn't introduce new model variables, so
+    # saving the base model can capture all variables in the model.
+    self.base_model.save(*args, **kwargs)
 
   def perturb_on_batch(self, x, **config_kwargs):
     """Perturbs the given input to generates adversarial examples.
