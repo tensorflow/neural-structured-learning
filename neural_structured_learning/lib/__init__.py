ff15f75be8b96ae28d2e2585d2c8080a4547f5c7
Chun-Sung Ferng <csferng@google.com>
2022-06-22 09:39:23 -0700

    Export `random_in_norm_ball` in `nsl.lib`.
    PiperOrigin-RevId: 456535765

--- neural_structured_learning/lib/__init__.py
+++ neural_structured_learning/lib/__init__.py
@@ -13,6 +13,7 @@ from neural_structured_learning.lib.utils import get_target_indices
 from neural_structured_learning.lib.utils import maximize_within_unit_norm
 from neural_structured_learning.lib.utils import normalize
 from neural_structured_learning.lib.utils import project_to_ball
+from neural_structured_learning.lib.utils import random_in_norm_ball
 from neural_structured_learning.lib.utils import replicate_embeddings
 from neural_structured_learning.lib.utils import strip_neighbor_features
 from neural_structured_learning.lib.utils import unpack_neighbor_features
@@ -30,6 +31,7 @@ __all__ = [
     'normalize',
     'pairwise_distance_wrapper',
     'project_to_ball',
+    'random_in_norm_ball',
     'replicate_embeddings',
     'strip_neighbor_features',
     'unpack_neighbor_features',
